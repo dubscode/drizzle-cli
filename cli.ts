16674d1098c59159aae33c4eaedc3692f0cfd679
Daniel Wise <Daniel.Wise@bwh.com>
2024-11-22 11:43:39 -0800

    chore: bump version 0.3.0

--- cli.ts
+++ cli.ts
@@ -11,7 +11,7 @@ import { generateValidators } from '@/generators/validators.ts';
 
 const cli = new Command()
   .name('drizzle-cli')
-  .version('0.2.0')
+  .version('0.3.0')
   .description('A CLI tool for scaffolding Drizzle ORM files');
 
 cli
